ef6ffef2657bca6104eb46c083996a2b594a7d07
idkjs <aarmand.inbox@gmail.com>
2017-11-20 14:17:06 +0100

    added reason dep files

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+/** plugin is also included in this configuration to 
+ * automatically add vendor prefixes for only the browsers 
+ * that need to be supported.  */
+module.exports = {
+  plugins: [require("autoprefixer")]
+};
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    root: "./src/index.re",
+    css: "./sass/styles.scss"
+  },
+  output: {
+    path: path.resolve(__dirname, "bundledOutputs"),
+    filename: "[name].js"
+  },
+  devServer: {
+    contentBase: "./bundledOutputs"
+  },
+  plugins: [
+    new CleanWebpackPlugin(["bundledOutputs"]),
+    new HtmlWebpackPlugin({
+      title: "ReasonML CSS-GRID",
+      template: "src/index.html"
+    }),
+    new ExtractTextPlugin("style.css")
+  ],
+  module: {
+    rules: [
+      // Set up Reason and OCaml files to use the loader
+      { test: /\.(re|ml)$/, use: "bs-loader" },
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                minimize:
+                  true ||
+                  {
+                    /* CSSNano Options */
+                  }
+              }
+            },
+            {
+              loader: "postcss-loader"
+            },
+            {
+              loader: "sass-loader"
+            }
+          ]
+        })
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              mimetype: "application/font-woff"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: "images/[hash]-[name].[ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [{ loader: "file-loader" }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".re", ".ml", ".js"]
+  }
+};
